5a94c2ddb0b91ba2fbe2994fa225fcf1d90452e6
Draigan <draigan.lefebvre@gmail.com>
2022-12-26 17:35:14 -0500

    built my own dropdown menu

--- js/submenu.js
+++ js/submenu.js
@@ -1,4 +1,26 @@
-const vw = Math.max(document.documentElement.clientWidth || 0, window.innerWidth || 0)
+const dropdown = document.querySelector("[data-dropdown]");
+const link = document.querySelector("[data-submenu-link]");
 
-console.log("HEY HEY HEY")
-console.log(vw)
+let isHidden = true
+dropdown.hidden = isHidden;
+function toggle() {
+  isHidden = !isHidden;
+  dropdown.hidden = isHidden;
+
+}
+link.addEventListener("click", () => {
+  toggle();
+})
+
+document.body.addEventListener("click", function() {
+  isHidden = true;
+  dropdown.hidden = true;
+});
+
+dropdown.addEventListener("click", function(ev) {
+  ev.stopPropagation();
+});
+
+link.addEventListener("click", function(ev) {
+  ev.stopPropagation();
+});
